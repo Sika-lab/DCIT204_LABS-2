ae33f9a040069b87c30fac895d73583a187b0a62
Sika-lab <sasampiah@st.ug.edu.gh>
2026-07-01 01:03:14 +0000

    Implement FindMax to find highest number in array

---
+++ Lab2/FindMax.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+ 
+public class FindMax{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = input.nextInt();
+
+        int[] numbers = new int[n];
+
+   
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = input.nextInt();
+        }
+
+        int highest = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > highest) {
+                highest = numbers[i];
+            }
+        }
+        System.out.println("The highest number in the array is: " + highest);
+
+        input.close();
+    }
+}
+
