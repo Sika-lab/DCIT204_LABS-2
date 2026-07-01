d8b6b02f835a1d51a7171d19b2ee490289b38506
Sika-lab <sasampiah@st.ug.edu.gh>
2026-07-01 02:10:10 +0000

    Update SumPair method to return found pair
    Refactor SumPair method to return pair of numbers and remove unused variables.

--- SumPair.java
+++ SumPair.java
@@ -17,23 +17,27 @@ public class SumPairFinder {
         System.out.print("Enter the target sum: ");
         int target = input.nextInt();
 
-        boolean found = false;
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (numbers[i] + numbers[j] == target) {
-                    System.out.println(
-                        "Pair found: " + numbers[i] + " + " + numbers[j] + " = " + target
-                    );
-                    found = true;
-                }
-            }
-        }
-
-        if (!found) {
+        int result=Sumpair(array, 20);
+        if (result==-1) {
             System.out.println("No pair found with sum " + target);
         }
 
         input.close();
     }
+public static int SumPair(int[] array, this.target){
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    
+                   return    (numbers[i] ,numbers[j] );
+                    
+                    
+                }
+            }
+        }
+        return -1;
+    }
+    
+
+
 }
